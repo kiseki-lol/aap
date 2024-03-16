0e3d118f42d96068dbe8e07d49037bc3f3ccdd15
rjindael <me@rjindael.xyz>
2024-03-16 00:41:56 -0700

    bug fixes

--- AAP/Program.cs
+++ AAP/Program.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 using System.Text;
 using System.Security.Cryptography;
 using Org.BouncyCastle.Crypto;
@@ -10,6 +12,20 @@ using MessagePack;
 using SharpCompress.Common;
 using SharpCompress.Writers;
 using ZstdSharp;
+
+[Serializable]
+[XmlRoot("Settings")]
+[MessagePackObject]
+public class AppSettings
+{
+    [XmlElement("ContentFolder")]
+    [Key(0)]
+    public string? ContentFolder { get; set; }
+
+    [XmlElement("BaseUrl")]
+    [Key(1)]
+    public string? BaseUrl { get; set; }
+}
 
 namespace AAP
 {
@@ -36,7 +52,18 @@ namespace AAP
 
             // Prepare the data
             string appSettingsPath = Path.Combine(resourceFolderPath, "AppSettings.xml");
-            byte[] appSettingsData = MessagePackSerializer.Serialize(File.ReadAllText(appSettingsPath));
+            var appSettingsXml = XDocument.Load(appSettingsPath);
+            var xmlSerializer = new XmlSerializer(typeof(AppSettings));
+            AppSettings? appSettings;
+            using (var reader = appSettingsXml.CreateReader())
+            {
+                appSettings = xmlSerializer.Deserialize(reader) as AppSettings;
+                if (appSettings == null)
+                {
+                    throw new InvalidOperationException("Failed to deserialize AppSettings.");
+                }
+            }
+            byte[] appSettingsData = MessagePackSerializer.Serialize(appSettings);
             ushort appSettingsSize = (ushort)appSettingsData.Length;
 
             string[] assetFolders = { "content", "PlatformContent", "shaders" };
@@ -46,7 +73,9 @@ namespace AAP
             // Create the signature
             ISigner signer = SignerUtilities.GetSigner("SHA-256withRSA");
             signer.Init(true, keyPair.Private);
+            signer.BlockUpdate(BitConverter.GetBytes(appSettingsSize), 0, sizeof(ushort));
             signer.BlockUpdate(appSettingsData, 0, appSettingsData.Length);
+            signer.BlockUpdate(BitConverter.GetBytes(archiveSize), 0, sizeof(ulong));
             signer.BlockUpdate(archiveData, 0, archiveData.Length);
             byte[] signature = signer.GenerateSignature();
 
@@ -69,7 +98,17 @@ namespace AAP
             {
                 foreach (var folder in folders)
                 {
-                    writer.Write(folder, Path.Combine(basePath, folder));
+                    var dirInfo = new DirectoryInfo(Path.Combine(basePath, folder));
+                    if (!dirInfo.Exists)
+                    {
+                        throw new DirectoryNotFoundException($"The directory {dirInfo.FullName} does not exist.");
+                    }
+
+                    foreach (var file in dirInfo.GetFiles("*", SearchOption.AllDirectories))
+                    {
+                        string relativePath = Path.GetRelativePath(basePath, file.FullName);
+                        writer.Write(relativePath, file.FullName);
+                    }
                 }
             }
 
